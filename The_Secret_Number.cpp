f34bf3057ffd13d0a69d652f7f834a54e78f4fcd
kiu-art <9227@ujwal>
2025-08-25 16:32:53 +0530

    The_Secret_Number

--- The_Secret_Number.cpp
+++ The_Secret_Number.cpp
@@ -1,41 +1,34 @@
-#include<bits/stdc++.h>
-using namespace std;
-int main(){
+#include <iostream> 
+#include <vector>   
+#include <algorithm> 
+using namespace std; 
+#define int long long
+void fast_io() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    std::cout.tie(NULL);
+}
+signed main(){
+  fast_io();
   int n;
   cin>>n;
   for(int x=0;x<n;x++){
-    vector<string> o;
-    int k=0;
-    string a;
-    cin>>a;
-    int e=0;
-    for(int y=0;y<=a.length()/2;y++){
-        if(a[y]!=a[a.length()-1-y]){
-            cout<<0;
-            e=1;
-            break;
+    int a;cin>>a;
+    vector<int> v;
+    for(int y=1;y<100;y++){
+        int d=1;
+        for(int z=0;z<y;z++){d*=10;}
+        d+=1;
+        int e=a%d;
+        if(e==0){
+            v.push_back(a/d);
         }
+        if(a/d<1){break;}
     }
-    if(e!=1 and a[a.length()/2]%a[0]==0){
-        for(int y=(a.length()/2);y>=1;y--){
-            string f="";
-            if(a.length()%y==0 and ((a.length()-y)/y)%2==1){
-                for(int z=0;z<(a.length()-y)/y;z++){
-                    if(z%2==0){
-                        f+=a.substr(0,y);
-                    }
-                    else{
-                        for(int j=0;j<y;j++){
-                            f+="0";
-                        }
-                    }
-                }
-                k++;
-                o.push_back(f);
-            }
-        }
-    }
-    if(o.size()!=0){cout<<k<<endl;for(auto x:o){cout<<x<<" ";}}
-    cout<<endl;
+    sort(v.begin(),v.end());
+    cout<<v.size()<<"\n";
+    if(v.size()!=0){
+    for(auto y:v){cout<<y<<" ";}
+    cout<<"\n";}
   }
 }
