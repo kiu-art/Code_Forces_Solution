49a42eddc69fe3e4441d30468902e3345eca8a88
kiu-art <9227@ujwal>
2025-09-05 15:34:14 +0530

    1845A_Forbidden_Integer

--- 1845A_Forbidden_Integer.cpp
+++ 1845A_Forbidden_Integer.cpp
@@ -13,12 +13,39 @@ int main() {
     fast_io(); 
     int t;cin>>t;
     while(t--){
-        int n,k,x;cin>>n>>k>>x;
         vector<int> v;
-        while(n>0){
-            n-=k;
-            v.push_back(k)
+        int n,k,a;cin>>n>>k>>a;
+        if(k==1){
+            cout<<"NO";
         }
+        else if(k==2 and a==1 and n%2==1){
+            cout<<"NO";
+        }
+        else if(a!=1){
+            cout<<"YES\n";
+            for(int x=0;x<n;x++){
+                v.push_back(1);
+            }
+            cout<<v.size()<<"\n";
+            for(auto f:v){cout<<f<<" ";}
+        }
+        else{
+            cout<<"YES\n";
+            if(n%2==0){
+                for(int x=1;x<=n;x+=2){
+                    v.push_back(2);
+                }
+            }
+            else{
+                for(int x=1;x<=n-3;x+=2){
+                    v.push_back(2);
+                }
+                v.push_back(3);
+            }
+            cout<<v.size()<<"\n";
+            for(auto f:v){cout<<f<<" ";}
+        }
+        cout<<"\n";
     }
     return 0;
 }
