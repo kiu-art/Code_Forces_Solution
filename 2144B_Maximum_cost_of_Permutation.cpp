ce27b01bceb5e8da616e5d18122c6760a69840be
kiu-art <9227@ujwal>
2025-09-18 22:02:43 +0530

    2144B_Maximum_cost_of_Permutation

--- 2144B_Maximum_cost_of_Permutation.cpp
+++ 2144B_Maximum_cost_of_Permutation.cpp
@@ -13,62 +13,37 @@ int main() {
     fast_io(); 
     int t;cin>>t;
     while(t--){
-        int a;
         int n;cin>>n;
         vector<int> v(n);
-        vector<int> r;
+        vector<int> r(n);
+        vector<int> a;
+        for(auto &x:v){cin>>x;r[x-1]=1;}
         for(int x=0;x<n;x++){
-            cin>>a;
-            v[x]=a;
-            if(a==0){r.push_back(x);}
-        }
-        if(count(v.begin(),v.end(),0)==n){
-            if(n==1){
-                cout<<0;
-                continue;
-            }
-            else{
-            cout<<n<<"\n";
-        continue;}
-            
-        }
-        vector<int> arr;
-        for(int x=1;x<=n;x++){
-            int u=0;
-            for(auto y:v){
-                if(x!=y){
-                    u++;
-                }
-                if(u==n){
-                    arr.push_back(x);
-                }
+            if(r[x]==0){
+                a.push_back(x+1);
             }
         }
-        int f=r.size()-1;
-        sort(arr.begin(),arr.end());
-        for(auto x:r){
-            v[x]=arr[f];
-            f--;
+        int b=a.size()-1;
+        sort(a.begin(),a.end());
+        for(auto &x:v){
+            if(x==0){
+                x=a[b];
+                b--;
+            }
         }
-        vector<int> v2(n);
-        v2=v;
         int g=0;
         int h=0;
-        int i=0;
-        sort(v2.begin(),v2.end());
+        int e=1;
         for(int x=0;x<n;x++){
-            if(v2[x]!=v[x] and h==0){
+            if(v[x]!=x+1 and e==1){
                 g=x;
-                h=1;
+                e=0;
             }
-            if(v2[x]!=v[x] and h==1){
-                i=x;
+            if(v[x]!=x+1){
+                h=x;
             }
         }
-        if(i==g){cout<<0<<"\n";}
-        else{
-        cout<<i-g+1<<"\n";
-        }
+        cout<<h-g+bool(h-g)<<"\n";
     }
     return 0;
 }
