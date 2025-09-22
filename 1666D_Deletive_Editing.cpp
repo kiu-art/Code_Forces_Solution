8b654704fb98a63106a988dab00326b0b4e0139f
kiu-art <9227@ujwal>
2025-09-22 17:13:42 +0530

    pata nahi kya galat hai

---
+++ 1666D_Deletive_Editing.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h> 
+using namespace std; 
+#define int long long
+
+void fast_io() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    std::cout.tie(NULL);
+}
+signed main() {
+    fast_io(); 
+    int t;cin>>t;
+    while(t--){
+        string s,a;cin>>s>>a;
+        int c=a.length();
+        int e=0;
+        while(e==0){
+            if(s.length()<=c){e=2;break;}
+            for(int x=0;x<s.length()-c and e!=1;x++){
+                if(s[x]==a[0]){
+                    s.erase(0,x);
+                    x=0;
+                }
+                for(int y=0;y<c;y++){
+                    if(s[x+y]!=a[y]){
+                        //cout<<s[x+y]<<"  ";
+                        s.erase(x+y,1);
+                        //cout<<s<<"  \n";
+                        x=-1;
+                        break;
+                    }
+                    if(y==c-1){
+                        e=1;
+                    }
+                }
+            }
+            if(e==1){
+                for(int x=c;x<s.length();x++){
+                    for(auto y:a){
+                        if(y==s[x]){
+                            e==0;
+                            s.erase(0,c);
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        if(s.substr(0,c)==a){cout<<"YES\n";}
+        else{cout<<"NO\n";}
+    }
+    return 0;
+}
