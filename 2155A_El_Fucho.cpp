8e9a93a868cd4d252de64b1d23958e0691003027
kiu-art <9227@ujwal>
2025-10-05 22:20:16 +0530

    2155A_El_Fucho

---
+++ 2155A_El_Fucho.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h> 
+using namespace std; 
+#define int long long
+
+void fast_io() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    std::cout.tie(NULL);
+}
+
+signed main() {
+    fast_io(); 
+    int t;cin>>t;
+    while(t--){
+        int n;cin>>n;
+        cout<<(n*2)-2<<endl;
+    }
+    return 0;
+}
