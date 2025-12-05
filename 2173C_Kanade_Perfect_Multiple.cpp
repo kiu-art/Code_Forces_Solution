b35d29c77ea6cc4af9d83b04ab5bb740dcf3d961
kiu-art <9227@ujwal>
2025-12-05 23:06:00 +0530

    2173C_Kanade_Perfect_Multiple

--- 2173C_Kanade_Perfect_Multiple.cpp
+++ 2173C_Kanade_Perfect_Multiple.cpp
@@ -7,50 +7,18 @@ void fast_io() {
     std::cin.tie(NULL);
     std::cout.tie(NULL);
 }
-int ma=2000100;
-vector<int> v(ma);
-vector<set<int>> num(ma);
-void sieve(){
-    for(int x=1;x<ma;x++){
-        v[x]=x;
-    }
-    for(int x=2;x<ma;x++){
-        if(v[x]==x){
-            for(int y=x*x;y<ma;y+=x){
-                if(v[y]==y){
-                    v[y]=x;
-                }
-            }
-        }
-    }
-}
-int factor(int n){
-    if(n<2){return 0;}
-    set<int> s;
-    int l=n;
-    while(n>=2){
-        s.insert(v[n]);
-        n/=v[n];
-    }
-    num[l].insert(s.begin(),s.end());
-    return 0;
-}
 signed main() {
     fast_io(); 
-    sieve();
     int t;cin>>t;
     while(t--){
         int n,k;cin>>n>>k;
         set<int>s;
         vector<int>arr(n);
         int g=0;
-        int h=0;
+        sort(arr.begin(),arr.end());
         for(auto &x:arr){
             cin>>x;
-            h+=x;
         }
-        sort(arr.begin(),arr.end());
-        if(arr[n-1]<=k and h==n*(n+1)/2 ){cout<<1<<endl<<1<<endl;continue;}
         for(auto x:arr){
             if(g==1){
                 break;
@@ -65,14 +33,8 @@ signed main() {
             if(e==0){
                 int q=0;
                 int l=0;
-                if(num[x].size()==0){
-                    factor(x);
-                }
-
-                for(auto y:num[x]){
-                    if(y>k){
-                        break;
-                    }
+                for(int y=1;y<=k or y<=x;y++){
+                    if(x%y==0){
                         int w=0;
                         for(int p=y;p<=k;p+=y){
                             if(count(arr.begin(),arr.end(),p)==0){
@@ -87,32 +49,13 @@ signed main() {
                             }
                         }
                         if(w>q){q=w;l=y;}
+                    }
                 }
                 if(l==0){g=1;}
                 else{s.insert(l);}
-                // for(int y=1;y<=k or y<=x;y++){
-                //     if(x%y==0){
-                //         int w=0;
-                //         for(int p=y;p<=k;p+=y){
-                //             if(count(arr.begin(),arr.end(),p)==0){
-                //                 w=-1;
-                //                 break;
-                //             }
-                //         }
-                //         if(w==-1){continue;}
-                //         for(auto j:arr){
-                //             if(j%y==0){
-                //                 w++;
-                //             }
-                //         }
-                //         if(w>q){q=w;l=y;}
-                //     }
-                // }
             }
         }
-        if(g==1){
-            cout<<-1<<endl;continue;
-        }
+        if(g==1){cout<<-1<<endl;continue;}
         cout<<s.size()<<endl;
         for(auto x:s){cout<<x<<" ";}
         cout<<endl;
