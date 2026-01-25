bf665ff497bb59fa8c4bed6eebe3d5e801ee108e
kiu-art <9227@ujwal>
2026-01-25 15:18:03 +0530

    1827A_Counting_Orders

--- 1827A_Counting_Orders.cpp
+++ 1827A_Counting_Orders.cpp
@@ -11,7 +11,7 @@ void fast_io() {
 signed main() {
     fast_io(); 
     int t;cin>>t;
-    while(t--){
+    for(int u=1;u<=t;u++){
         int n;cin>>n;
         vector<int>a(n);
         vector<int>b(n);
@@ -21,10 +21,39 @@ signed main() {
         sort(b.begin(),b.end());
         int r=1;
         int c=0;
+        int e=0;
+        int f=n;
+        int d=n-1;
         for(int x=n-1;x>=0;x--){
             c=b[x];
-            
+            if(f!=0){
+                for(int y=d;y>=0;y--){
+                    if(a[y]>c){
+                        e++;
+                        f=y;
+                    }
+                    else{
+                        d=y;
+                        break;
+                    }
+                }
+            }
+            // cout<<e<<" ";
+            r*=e;
+            r%=1000000007;
+            e--;
         }
+        // if(u==3445){
+        //     cout<<n<<"??";
+        //     for(auto x:a){
+        //         cout<<x<<"/";
+        //     }
+        //     cout<<"///";
+        //     for(auto x:b){
+        //         cout<<x<<"/";
+        //     }
+        // }
+        cout<<r<<endl;
     }
     return 0;
 }
