4395e567d9a52042c27b491af92405a9410b9cd8
kiu-art <9227@ujwal>
2026-01-25 08:47:16 +0530

    1842B_Tenizing_and_Books

--- 1842B_Tenizing_and_Books.cpp
+++ 1842B_Tenizing_and_Books.cpp
@@ -1,3 +1,83 @@
+// #include <bits/stdc++.h> 
+// using namespace std; 
+// #define int long long
+
+// void fast_io() {
+//     std::ios_base::sync_with_stdio(false);
+//     std::cin.tie(NULL);
+//     std::cout.tie(NULL);
+// }
+
+// signed main() {
+//     fast_io(); 
+//     int t;cin>>t;
+//     while(t--){
+//         int n,m;cin>>n>>m;
+//         deque<int>r;
+//         int p=0;
+//         while(m){
+//             r.push_front(m%2);
+//             if(r[0]==1){
+//                 p++;
+//             }
+//             m/=2;
+//         }
+//         // r.push_back(1);
+//         // for(auto x:r){
+//         //     cout<<x<<" ";
+//         // }
+//         // cout<<endl;
+//         set<int>s;
+//         int e=0;
+//         for(int x=1;x<=3*n;x++){
+//             int a;cin>>a;
+//             if(e==1){
+//                 continue;
+//             }
+//             set<int>h;
+//             for(int y=0;y<33 and a!=0;y++){
+//                 // if(a==0){
+//                 //     if(y>=r.size()){
+//                 //         e=1;
+//                 //         break;
+//                 //     }
+//                 //     else if(r[y]==1){
+//                 //         h.insert(y);
+//                 //     }
+//                 //     else{
+//                 //         e=1;
+//                 //         break;
+//                 //     }
+//                 // }
+//                 if(a%2==1){
+//                     if(r[y]==1){
+//                         h.insert(y);
+//                     }
+//                     else{
+//                         e=1;
+//                         break;
+//                     }
+//                 }
+//                 a/=2;
+//             }
+//             if(!e){
+//                 s.insert(h.begin(),h.end());
+//             }
+//             if(x%n==0){
+//                 e=0;
+//             }
+//         }
+//         if(s.size()==p){
+//             cout<<"Yes"<<endl;
+//         }
+//         else{
+//             cout<<"No"<<endl;
+//         }
+//     }
+//     return 0;
+// }
+
+
 #include <bits/stdc++.h> 
 using namespace std; 
 #define int long long
@@ -13,44 +93,45 @@ signed main() {
     int t;cin>>t;
     while(t--){
         int n,m;cin>>n>>m;
-        vector<int>r;
-        vector<int>v;
-        while(r){
-            r[x].push_back(r%2);
-            r/=2;
-        }
-        for(int x=v.size()-1;x>=0;x--){
-            if(r[x]==0){
-                v.push_back(x);
-            }
-        }
-        vector<pair<int,int>>v1(n);
-        vector<pair<int,int>>v2(n);
-        vector<pair<int,int>>v3(n);
+        int d,e;
+        d=e=0;
         for(int x=0;x<n;x++){
-            int a;cin>>a;
-            while(a){
-                v1[x].push_back(a%2);
-                a/=2;
+            int y;cin>>y;
+            if(e==1){continue;}
+            else if((m|y)!=m){
+                e=1;
+            }
+            else{
+                d|=y;
             }
         }
+        e=0;
         for(int x=0;x<n;x++){
-            int a;cin>>a;
-            while(a){
-                v2[x].push_back(a%2);
-                a/=2;
+            int y;cin>>y;
+            if(e==1){continue;}
+            else if((m|y)!=m){
+                e=1;
+            }
+            else{
+                d|=y;
             }
         }
+        e=0;
         for(int x=0;x<n;x++){
-            int a;cin>>a;
-            while(a){
-                v3[x].push_back(a%2);
-                a/=2;
+            int y;cin>>y;
+            if(e==1){continue;}
+            else if((m|y)!=m){
+                e=1;
+            }
+            else{
+                d|=y;
             }
         }
-        int e=0;
-        while(e){
-            if(v[x])
+        if(d==m){
+            cout<<"Yes"<<endl;
+        }
+        else{
+            cout<<"No"<<endl;
         }
     }
     return 0;
